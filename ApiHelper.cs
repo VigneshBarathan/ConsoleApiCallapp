26a8a273df86a3ce4c9dbd2e48f58743b4dc9786
Vignesh Barathan <vignesh.barathan@sisystems.com>
2022-01-09 20:06:18 +0530

    Add project files.

---
+++ ApiHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace ConsoleApiCallapp
+{
+    public class ApiHelper
+    {
+        public async Task<ChuckNorrisJoke> ApiCall()
+        {
+            var services = new ServiceCollection();
+            services.AddHttpClient<ChuckNorrisClient>(c => c.BaseAddress = new Uri("https://api.chucknorris.io/"));
+            var serviceProvider = services.BuildServiceProvider();
+
+            var client = serviceProvider.GetService<ChuckNorrisClient>();
+            var response =  client.GetRandomJokeAsync();
+
+            return await response;
+        }
+    }
+
+    public record ChuckNorrisJoke(string Id, string Value, string Url);
+    
+    public class ChuckNorrisClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChuckNorrisClient(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async Task<ChuckNorrisJoke> GetRandomJokeAsync() => await _httpClient.GetFromJsonAsync<ChuckNorrisJoke>("jokes/random");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApiCallapp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ApiHelper apiHelper = new();
+            var obj = await apiHelper.ApiCall();
+
+            System.Console.WriteLine($"Id: {obj.Id}");
+            System.Console.WriteLine($"Joke: {obj.Value}");
+
+        }
+    }
+}
